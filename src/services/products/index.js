97db1c00c6f2d6f89af86b9c136c9a0ceeb9239b
Carlo Lombardi <carlo_lombardi@users.noreply.github.com>
2021-04-30 10:14:35 +0100

    Post and Get added

--- src/services/products/index.js
+++ src/services/products/index.js
@@ -5,4 +5,37 @@ import ProductsModel from "./schema.js";
 
 const route = express.Router();
 
+route.get("/", async (req, res, next) => {
+  try {
+    const products = await ProductsModel.find();
+    res.send(products);
+  } catch (err) {
+    next(err);
+  }
+});
+
+route.get("/:id", async (req, res, next) => {
+  try {
+    const products = await ProductsModel.findById(req.params.id);
+    if (products) {
+      res.send(products);
+    } else {
+      const error = new Error("products not found");
+      error.httpStatusCode = 404;
+      next(error);
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+route.post("/", async (req, res, next) => {
+  try {
+    const newProduct = new ProductsModel(req.body);
+    const { _id } = await newProduct.save();
+    res.status(201).send(_id);
+  } catch (err) {
+    next(err);
+  }
+});
 export default route;
