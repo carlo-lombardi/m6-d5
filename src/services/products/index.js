4c36d8aefd2ca14bfb37e68e620932938e4d5c43
Carlo Lombardi <carlo_lombardi@users.noreply.github.com>
2021-04-30 10:26:30 +0100

    Delete and Put added

--- src/services/products/index.js
+++ src/services/products/index.js
@@ -38,4 +38,41 @@ route.post("/", async (req, res, next) => {
     next(err);
   }
 });
+
+route.delete("/:id", async (req, res, next) => {
+  try {
+    const product = await ProductsModel.findByIdAndDelete(req.params.id);
+    if (product) {
+      res.send("product deleted!");
+    } else {
+      const error = new Error(`products with id ${req.params.id} not found`);
+      error.httpStatusCode = 404;
+      next(error);
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+route.put("/:id", async (req, res, next) => {
+  try {
+    const product = await ProductsModel.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        runValidators: true,
+        new: true,
+      }
+    );
+    if (product) {
+      res.send(product);
+    } else {
+      const error = new Error(`product with id ${req.params.id} not found`);
+      error.httpStatusCode = 404;
+      next(error);
+    }
+  } catch (err) {
+    next(err);
+  }
+});
 export default route;
